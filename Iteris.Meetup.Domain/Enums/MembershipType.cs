a61b673a65b16f52323c1c1ed5156c1a0b58287e
Lucas Domingues <lucas.h.domingues@hotmail.com>
2021-03-21 20:30:44 -0300

    Inclui classes fora do src

--- Iteris.Meetup.Domain/Enums/MembershipType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Iteris.Meetup.Domain.Enums
-{
-    public enum SubscriptionType : byte
-    {
-        Monthly = 0,
-        Annual = 1,
-        Lifetime = 2
-    }
-}
--- Iteris.Meetup.Domain/Interfaces/Repositories/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Iteris.Meetup.Domain.Entities;
-
-namespace Iteris.Meetup.Domain.Interfaces.Repositories
-{
-    public interface IUserRepository
-    {
-        User GetById(int userId);
-    }
-}
